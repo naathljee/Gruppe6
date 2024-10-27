db5669fe6c00038a5b5e892293e6c5d4e177a93c
TheGamingNorwegian <111729475+TheGamingNorwegian@users.noreply.github.com>
2024-10-27 20:19:35 +0100

    mer bugfixes og styling

--- DarkMode.js
+++ DarkMode.js
@@ -1,5 +1,8 @@
 const toggleButton = document.getElementById('darkModeToggle');
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('main').classList.toggle('dark-mode');
+    document.getElementById('title-collage').classList.toggle('dark-mode');
+    document.getElementById('text-collage').classList.toggle('dark-mode');
 });
-
